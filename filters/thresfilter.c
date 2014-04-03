4ae4a32e7993195f09146a4ca50e8c57e6e81b7c
mosterann <jacsu246@student.liu.se>
2014-04-03 13:30:21 +0200

    Hello world example added

--- filters/thresfilter.c
+++ filters/thresfilter.c
@@ -5,19 +5,23 @@ void thresfilter(const int xsize, const int ysize, pixel* src){
 
   uint sum, i, psum, nump;
 
-  nump = xsize * ysize;
+  nump = xsize * ysize; //Area
 
   for(i = 0, sum = 0; i < nump; i++) {
     sum += (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
   }
 
-  sum /= nump;
+  // sum = sum / nump;
+  sum /= nump; // Average pixel color
 
   for(i = 0; i < nump; i++) {
     psum = (uint)src[i].r + (uint)src[i].g + (uint)src[i].b;
+    
+    // If darker than average pixel color ---> Make pixel black
     if(sum > psum) {
       src[i].r = src[i].g = src[i].b = 0;
     }
+    // Brighter than average pixel color --> Make white
     else {
       src[i].r = src[i].g = src[i].b = 255;
     }
---
+++ hello_world/main.c
@@ -0,0 +1,23 @@
+#include "mpi.h"
+
+void main( void)
+{
+  MPI_Status status;
+  char *string = "xxxxx"; // Recieve buffer
+  int myid;
+
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  
+  if(myid==2)
+    MPI_Send( "HELLO", 5, MPI_CHAR, 7, 1234, MPI_COMM_WORLD);
+  
+  if(myid==7)
+    {
+      MPI_Recv(string, 5, MPI_CHAR, 2, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      printf( "Got %s from P%d, tag %d\n", string, status.MPI_SOURCE, status.MPI_TAG);
+
+    }
+  MPI_Finalize();
+
+}
