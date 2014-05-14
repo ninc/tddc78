21b2626d865941d23be3b6859fd709c2dc022b9b
Mats Ferdeen <x_matfe@triolith1.(none)>
2014-05-14 15:47:07 +0200

    Started outlining main.cpp in lab4

---
+++ lab4/main.cpp
@@ -0,0 +1,55 @@
+// Mädz super duper particle simulation (MSDPS)
+
+
+#include <iostream>
+#include "physics.h"
+#include "definitions.h"
+#include "coordinate.h"
+
+
+//Setup needed data structes for the simulation
+bool init_particles()
+{
+
+  return true;
+}
+
+
+// Simulate particle pressure
+int simulate()
+{
+
+  //FOR ALL PARTICLES
+
+  //Check for collisions
+
+  //Move particles that havn't colided
+  
+  // Check for wall interaction and add the momentum
+
+  // Communicate if needed
+
+  return 0;
+}
+
+
+
+// Calculate the pressure and verify that the calculations are correct
+bool calc_pressure()
+{
+
+  return true;
+}
+
+int main (int argc, char ** argv) {
+
+
+  // Initialize simulation
+  init_particles();
+
+  simulate();
+
+  calc_pressure();
+
+  return 0;
+}
